59275f8b3cb81c9738b022e6d8d6f44d1c659763
tylerlngu@csu.fullerton.edu <root@CSUF-1629312566.localdomain>
2024-11-27 11:00:37 -0800

    Large chunks tests

--- memtest.c
+++ memtest.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <time.h>
 
-// define "x" for system malloc, include for our versions. Don't do both.
 #ifdef SYSTEM_MALLOC
 #define xfree free
 #define xmalloc malloc
+#define xrealloc realloc
 #else
 #include "malloc.h"
 #endif
@@ -14,6 +14,8 @@
 int rand_between(int min, int max) { return rand() % (max - min + 1) + min; }
 
 #define TEST_SIZE 30
+#define LARGE_MIN 1024
+#define LARGE_MAX (1024 * 1024)
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
@@ -28,45 +30,62 @@ int main(int argc, char *argv[]) {
     test_string = argv[1];
   }
 
-  char *ptrs[TEST_SIZE];
+  char *ptrs[TEST_SIZE] = {0};
 
   for (int ix = 0; ix < TEST_SIZE; ix++) {
-    int size = rand_between(1, strlen(test_string) + 1);
-    fprintf(stderr, "\n\n\n[%d] size: %d\n", ix, size);
+    int size;
+    if (rand() % 5 == 0) {
+      size = rand_between(LARGE_MIN, LARGE_MAX);
+    } else {
+      size = rand_between(1, strlen(test_string) + 1);
+    }
 
+    fprintf(stderr, "\n[%d] Allocating size: %d\n", ix, size);
     ptrs[ix] = xmalloc(size);
-    if (ptrs[ix] == NULL) {
-      printf("[%d] malloc failed\n", ix);
+    if (!ptrs[ix]) {
+      fprintf(stderr, "[%d] malloc failed\n", ix);
       exit(1);
     }
 
     int len_to_copy = MIN(strlen(test_string), size - 1);
-
-    // fprintf(stderr, "[%d] ptrs[%d]: %p, going to copy %d chars\n", ix, ix,
-    //         ptrs[ix], len_to_copy);
-
     strncpy(ptrs[ix], test_string, len_to_copy);
     ptrs[ix][len_to_copy] = '\0';
+    fprintf(stderr, "[%d] Allocated: %p, Content: '%s'\n", ix, ptrs[ix],
+            ptrs[ix]);
 
-    fprintf(stderr, "[%x] '%s'\n", ix, ptrs[ix]);
+    if (rand() % 3 == 0) {
+      int new_size = rand_between(1, size * 2);
+      fprintf(stderr, "[%d] Reallocating %p to new size: %d\n", ix, ptrs[ix],
+              new_size);
+      char *new_ptr = xrealloc(ptrs[ix], new_size);
+      if (new_ptr) {
+        ptrs[ix] = new_ptr;
+        int new_len_to_copy = MIN(strlen(test_string), new_size - 1);
+        strncpy(ptrs[ix], test_string, new_len_to_copy);
+        ptrs[ix][new_len_to_copy] = '\0';
+        fprintf(stderr, "[%d] Reallocated: %p, Content: '%s'\n", ix, ptrs[ix],
+                ptrs[ix]);
+      } else {
+        fprintf(stderr, "[%d] Realloc failed\n", ix);
+      }
+    }
 
     int index_to_free = rand_between(0, ix);
     if (ptrs[index_to_free]) {
-      fprintf(stderr, "\n[%d] randomly freeing %p ('%s')\n", index_to_free,
+      fprintf(stderr, "[%d] Randomly freeing %p ('%s')\n", index_to_free,
               ptrs[index_to_free], ptrs[index_to_free]);
       xfree(ptrs[index_to_free]);
-      fprintf(stderr, "[%d] freed %p\n", index_to_free, ptrs[index_to_free]);
       ptrs[index_to_free] = NULL;
     }
   }
 
   for (int ix = 0; ix < TEST_SIZE; ix++) {
     if (ptrs[ix]) {
-      fprintf(stderr, "[%d] freeing %p (%s)\n", ix, ptrs[ix], ptrs[ix]);
+      fprintf(stderr, "[%d] Freeing %p ('%s')\n", ix, ptrs[ix], ptrs[ix]);
       xfree(ptrs[ix]);
-      fprintf(stderr, "[%d] freed %p\n", ix, ptrs[ix]);
+      ptrs[ix] = NULL;
     } else {
-      fprintf(stderr, "[%d] already freed\n", ix);
+      fprintf(stderr, "[%d] Already freed\n", ix);
     }
   }
 
